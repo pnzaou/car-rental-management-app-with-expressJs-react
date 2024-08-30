6d47ead7ed09540be3eec2fd3225a39c38b26500
pnzaou <perrinemmanuelnzaou@gmail.com>
2024-08-30 13:44:22 +0000

    Modification de la fonction updateVoiture

--- server/src/controllers/voiture.controller.js
+++ server/src/controllers/voiture.controller.js
@@ -3,6 +3,23 @@ const VUT = require('../models/voitureUniteTarification.model')
 const VOL = require('../models/voitureOptionLocation.model')
 const { deleteLogo } = require('../services')
 
+/**
+ * Ajoute une nouvelle voiture avec ses options et tarifs de location.
+ * @async
+ * @param {Object} req - L'objet de requête Express.
+ * @param {Object} res - L'objet de réponse Express.
+ * @param {string} req.body.immatriculation - L'immatriculation de la voiture.
+ * @param {Date} req.body.DateMiseCirculation - La date de mise en circulation de la voiture.
+ * @param {string} req.body.typeCarburant - Le type de carburant de la voiture.
+ * @param {number} req.body.capaciteDassise - La capacité d'assise de la voiture.
+ * @param {string} req.body.categorieId - L'identifiant de la catégorie de la voiture.
+ * @param {string} req.body.modeleId - L'identifiant du modèle de la voiture.
+ * @param {Array} req.body.selectedOptions - Les options sélectionnées pour la voiture.
+ * @param {string} req.body.uniteTarificationId - L'identifiant de l'unité de tarification.
+ * @param {number} req.body.tarifLocation - Le tarif de location de la voiture.
+ * @param {Array} req.files - Les images de la voiture.
+ * @returns {Promise<void>} Renvoie une réponse JSON avec un message de succès ou d'erreur.
+ */
 const addVoiture = async (req, res) => {
     const { 
         immatriculation, 
@@ -50,6 +67,13 @@ const addVoiture = async (req, res) => {
     }   
 }
 
+/**
+ * Récupère toutes les voitures.
+ * @async
+ * @param {Object} req - L'objet de requête Express.
+ * @param {Object} res - L'objet de réponse Express.
+ * @returns {Promise<void>} Renvoie une réponse JSON avec la liste des voitures récupérées.
+ */
 const getVoitues = async (req, res) => {
     try {
         const rep = await Voiture.find()
@@ -61,6 +85,7 @@ const getVoitues = async (req, res) => {
     }
 }
 
+
 const updateVoiture = async (req, res) => {
     const { id } = req.params
     const { 
@@ -69,12 +94,11 @@ const updateVoiture = async (req, res) => {
         typeCarburant, 
         capaciteDassise, 
         categorieId, 
-        modeleId 
+        modeleId,
+        tarifLocation
     } = req.body
     
     try {
-        const voiture = await Voiture.findById(id)
-        const {images} = voiture
 
         const updateData = {
             immatriculation, 
@@ -90,24 +114,41 @@ const updateVoiture = async (req, res) => {
             updateData.images = images
         }
 
-        const rep = await Voiture.updateOne({_id: id}, {$set: updateData})
+        const rep = await Voiture.findByIdAndUpdate(id, updateData, {new: true})
 
-        if(req.files && rep.modifiedCount > 0) {
-            images.map(image => {
-                try {
-                    deleteLogo(image)
-                } catch (error) {
-                    console.log("Erreur lors de la suppression des anciennes images: ", error.message)
-                }
-            })
-        }
+        await VUT.updateOne({voitureId: id}, {$set: {tarifLocation}})
+
+        const selectedOptions = JSON.parse(req.body.selectedOptions)
+        const existingOptions = await VOL.find({voitureId: id})
+
+        const optionsToRemove = existingOptions.filter(opt => 
+            !selectedOptions.some(selected => selected.optionLocationId === String(opt.optionLocationId))
+        )
+
+        const removePromises = optionsToRemove.map(option => 
+            VOL.findByIdAndDelete(option._id)
+        )
+
+        await Promise.all(removePromises)
+
+        const addOrUpdatePromises = selectedOptions.map(option => {
+            return VOL.findOneAndUpdate(
+                {voitureId: id, optionLocationId: option.optionLocationId},
+                {tarifOption: option.tarifOption},
+                {upsert: true, new: true}
+            )
+        })
+
+        await Promise.all(addOrUpdatePromises)
 
         const msg = "Voiture modifiée avec succès"
         return res.status(200).json({message: msg, data: rep})
 
     } catch (error) {
+
         const msg = "Erreur lors de la modification"
         return res.status(500).json({message: msg, erreur: error})
+
     }
 }
 
